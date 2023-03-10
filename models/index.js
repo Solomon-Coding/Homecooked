f1650530ac6811b0552e1573c6d34ca3a6b88689
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-03-09 20:09:47 -0600

    Edited and added to the index file containing the models and their associations.

--- models/index.js
+++ models/index.js
@@ -1,3 +1,41 @@
 const User = require('./User');
+const Recipes = require('./Recipes');
+const Category = require('./Category');
+const Style = require('./Style');
 
-module.exports = { User };
+// Every recipe falls within a category
+Recipes.belongsTo(Category,
+    {
+      foreignKey: 'category_id',
+      onDelete: 'CASCADE',
+    }
+  );
+  
+  // Every category can refer to many recipe's
+  Category.hasMany(Recipes,
+    {
+      foreignKey: 'category_id',
+    }
+  );
+  
+// Every recipe falls within a particular food style
+  Recipes.belongsTo(Style,
+    {
+      foreignKey: 'category_id',
+      onDelete: 'CASCADE',
+    }
+  );
+
+// Every style can refer to many recipe's
+Style.hasMany(Recipes,
+    {
+      foreignKey: 'category_id',
+    }
+  );
+
+module.exports = {
+    User,
+    Recipes,
+    Category,
+    Style,
+  };
