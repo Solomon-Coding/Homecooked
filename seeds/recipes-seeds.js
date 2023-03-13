80df47081bb14106a554ce56b67353a2af70bcd6
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-03-13 00:18:32 -0500

    added routes for the data

--- seeds/recipes-seeds.js
+++ seeds/recipes-seeds.js
@@ -9,6 +9,22 @@ const recipesData = [
     category_id: 2,
     style_id: 1,
   },
+  {
+    name: 'Burger',
+    author:'Ummmm', 
+    instructions:'Cokka da meata',
+    ingredients: 'Sass',
+    category_id: 2,
+    style_id: 1,
+  },
+  {
+    name: 'Burger',
+    author:'Ummmm', 
+    instructions:'Cokka da meata',
+    ingredients: 'Sass',
+    category_id: 6,
+    style_id: 2,
+  },
 ];
 
 const seedRecipes = () => Recipes.bulkCreate(recipesData);
