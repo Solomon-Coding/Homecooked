e770dd8702fae2538bb4ec6cea3d9b5d49aaa6b2
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-03-13 21:10:44 -0500

    Worked on integrating routes with handlebar and models

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
+const recipeRoutes = require('./recipes-routes');
+
 
 router.use('/users', userRoutes);
+router.use('/recipes', recipeRoutes);
 
 module.exports = router;
---
+++ controllers/api/recipes-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const Recipes = require('../../models/Recipes');
+
+// route to create/add a recipes using async/await
+router.post('/', async (req, res) => {
+  try { 
+    const recipesData = await Recipes.create({
+    name: req.body.name,
+    author: req.body.author,
+    instructions: req.body.instructions,
+    ingredients: req.body.ingredients,
+    category_id: req.body.category_id,
+    style_id: req.body.style_id,
+  });
+  // if the recipes is successfully created, the new response will be returned as json
+  res.status(200).json(recipesData)
+} catch (err) {
+  res.status(400).json(err);
+}
+});
+
+
+module.exports = router;
